48ddf2be9b2cfb2b242845d7c8f54d83d1a8f9f8
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-15 14:39:16 +0300

    new version

--- MusicVideo/MusicVideo/AppDelegate.h
+++ MusicVideo/MusicVideo/AppDelegate.h
@@ -7,11 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Reachability.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (strong, nonatomic) Reachability *reachability;
+@property (strong, nonatomic) Reachability *internetCheck;
+@property (strong, nonatomic) NSString *reachabilityStatus;
 
 @end
-
