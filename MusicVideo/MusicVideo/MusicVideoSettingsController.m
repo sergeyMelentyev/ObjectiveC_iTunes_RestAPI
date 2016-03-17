2af2de5c471c3a457b1beb96ae5d97d2cadb22b7
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-17 19:01:44 +0300

    new version

--- MusicVideo/MusicVideo/MusicVideoSettingsController.m
+++ MusicVideo/MusicVideo/MusicVideoSettingsController.m
@@ -17,9 +17,46 @@
 @property (weak, nonatomic) IBOutlet UISwitch *hdImage;
 @property (weak, nonatomic) IBOutlet UILabel *apiCount;
 @property (weak, nonatomic) IBOutlet UISlider *sliderCount;
-
+- (IBAction)touchIdSecurity:(UISwitch *)sender;
+- (IBAction)valueChanged:(UISlider *)sender;
 @end
 
 @implementation MusicVideoSettingsController
 
+-(void) viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Settings";
+    self.touchID.on = [[NSUserDefaults standardUserDefaults] boolForKey:@"SecSettings"];
+    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"APICNT"] != nil) {
+        NSNumber *theValue = [[NSUserDefaults standardUserDefaults] objectForKey:@"APICNT"];
+        self.sliderCount.value = [theValue floatValue];
+        self.apiCount.text = [NSString stringWithFormat:@"%@", [NSNumber numberWithInt:(int)self.sliderCount.value]];
+    }
+}
+
+- (IBAction)touchIdSecurity:(UISwitch *)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if (self.touchID.on) {
+        [defaults setBool:self.touchID.on forKey:@"SecSettings"];
+    } else {
+        [defaults setBool:false forKey:@"SecSettings"];
+    }
+}
+
+- (IBAction)valueChanged:(UISlider *)sender {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:([NSNumber numberWithFloat:(float)self.sliderCount.value]) forKey:@"APICNT"];
+    self.apiCount.text = [NSString stringWithFormat:@"%@", [NSNumber numberWithInt:(int)self.sliderCount.value]];
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
