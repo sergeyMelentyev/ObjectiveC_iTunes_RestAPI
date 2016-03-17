c849cd4c0e7bdc1c75b17adb1dca2eeac354ee11
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-03-17 17:26:07 +0300

    new version

---
+++ MusicVideo/MusicVideo/MusicVideoSettingsController.h
@@ -0,0 +1,13 @@
+//
+//  MusicVideoSettingsController.h
+//  MusicVideo
+//
+//  Created by Админ on 17.03.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicVideoSettingsController : UITableViewController
+
+@end
---
+++ MusicVideo/MusicVideo/MusicVideoSettingsController.m
@@ -0,0 +1,25 @@
+//
+//  MusicVideoSettingsController.m
+//  MusicVideo
+//
+//  Created by Админ on 17.03.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import "MusicVideoSettingsController.h"
+
+@interface MusicVideoSettingsController()
+@property (weak, nonatomic) IBOutlet UILabel *aboutDisplay;
+@property (weak, nonatomic) IBOutlet UILabel *feedBackDisplay;
+@property (weak, nonatomic) IBOutlet UILabel *securityDisplay;
+@property (weak, nonatomic) IBOutlet UISwitch *touchID;
+@property (weak, nonatomic) IBOutlet UILabel *bestImageDisplay;
+@property (weak, nonatomic) IBOutlet UISwitch *hdImage;
+@property (weak, nonatomic) IBOutlet UILabel *apiCount;
+@property (weak, nonatomic) IBOutlet UISlider *sliderCount;
+
+@end
+
+@implementation MusicVideoSettingsController
+
+@end
